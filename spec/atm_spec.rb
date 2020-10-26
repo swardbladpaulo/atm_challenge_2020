493880d8fb13a892a9697178a66b21b1eb43c504
psnoya <paulina.s.noya@gmail.com>
2020-10-26 20:07:44 +0100

    example test

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -5,7 +5,7 @@ describe Atm do
     end
     
     it 'funds are reduced at withdraw' do
-        subject.withdraw 50
+        subject.withdraw(50, account)
         expect(subject.funds).to eq 950
     end
 end
@@ -17,9 +17,9 @@ describe Atm do
         allow(account).to receive(:balance).and_return(100)
         allow(account).to receive(:balance=) 
     end
-
+    
     it 'allow withdraw if the account has enough balance.' do
         expected_output = { status: true, message: 'success', date: Date.today, amount: 45  }
         expect(subject.withdraw(45, account)).to eq expected_output
-    end
+    end   
 end
