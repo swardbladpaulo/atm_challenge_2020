814f4a3453592cb743c1c37a2633c3f86b171ad7
swardbladpaulo <paulo@swardblads.se>
2020-10-27 19:25:39 +0100

    Pulling down from Paulinas repo

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,7 @@
 require './lib/atm.rb'
 
 describe Atm do
-    let (:account) { instance_double('Account', pin_code: '1234', exp_date: '04/17') }
+    let (:account) { instance_double('Account', pin_code: '1234', exp_date: '04/22') }
 
     before do
         allow(account).to receive(:balance).and_return(100)
