f1fa2010d489a2abd1eb55d0698ecbb8335665d4
psnoya <paulina.s.noya@gmail.com>
2020-10-27 19:23:33 +0100

    Fixed the card expiration date error by switching the date to 22

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,6 @@
 require './lib/atm.rb'
 describe Atm do
-    let (:account) { instance_double('Account', pin_code: '1234', exp_date: '04/17') }
+    let (:account) { instance_double('Account', pin_code: '1234', exp_date: '04/22') }
 
     before do
         allow(account).to receive(:balance).and_return(100)
@@ -41,6 +41,6 @@ describe Atm do
     it 'reject withdraw if the card is expired' do
         allow(account).to receive(:exp_date).and_return('12/15')
         expected_output = { status: false, message: 'card expired', date: Date.today }
-        expect(subject.withdraw(6, '1234', account)).to eq_expected_output
+        expect(subject.withdraw(6, '1234', account)).to eq expected_output
     end
 end
