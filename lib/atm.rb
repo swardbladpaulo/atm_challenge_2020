6552244c30800f06c07d58afd0687ab89f029551
swardbladpaulo <paulo@swardblads.se>
2020-10-27 12:01:34 +0100

    Test failed, push to review

--- lib/atm.rb
+++ lib/atm.rb
@@ -15,13 +15,18 @@ class Atm
         { status: false, message: 'insufficient funds in ATM', date: Date.today }
       when incorrect_pin?(pin_code, account.pin_code)
         {status: false, message: 'wrong pin', date: Date.today }
-    else
+      when card_expired?(account.exp_date)
+        { status: false, message: 'card expired', date: Date.today }
+      else
         perform_transaction(amount, account)
       end
     end
 
     private
 
+    def card_expired?(exp_date)
+      Date.strptime(exp_date, '%m/&y') < Date.today
+    end
 
     def incorrect_pin?(pin_code, actual_pin)
         pin_code != actual_pin
