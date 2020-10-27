1a3b6daade2b4098bcfa1b66c17c1a4ebb6e9798
swardbladpaulo <paulo@swardblads.se>
2020-10-27 09:54:59 +0100

    added method, insufficient funds in atm, run rspec, all good

--- lib/atm.rb
+++ lib/atm.rb
@@ -11,6 +11,8 @@ class Atm
       case
       when insufficient_funds_in_account?(amount, account)
       { status: false, message: 'insufficient funds', date: Date.today }
+      when insufficient_funds_in_atm?(amount)
+      { status: false, message: 'insufficient funds in ATM', date: Date.today }
       else
         perform_transaction(amount, account)
       end
@@ -20,6 +22,10 @@ class Atm
 
     def insufficient_funds_in_account?(amount, account)
         amount > account.balance
+    end
+
+    def insufficient_funds_in_atm?(amount)
+      @funds < amount
     end
 
     def perform_transaction(amount, account)
