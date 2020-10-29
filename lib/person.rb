f42a178249e282594e7f823ed7dc0e6a04116322
flis_ann <ann.flismark@gmail.com>
2020-10-29 13:42:35 +0100

    Adding method fot deposit_funds setting @account and @cash making two more tests

--- lib/person.rb
+++ lib/person.rb
@@ -10,19 +10,19 @@ class Person
     end
     
     def create_account
-        @account = Account.new(owner: self) 
+        @account = Account.new(owner: self) # 
     end
 
     def deposit(amount)
-        @account == nil ? missing_account : withdraw_funds(args)
+        @account == nil ? missing_account : deposit_funds(amount) # 1. is account ok? 2. If not go to missing account 3. else go to def deposit_funds
     end
-
 
     private 
 
-    #def deposit(amount)
-    #    @account = 
-    #end
+    def deposit_funds(amount)
+       @account.balance += amount # same account as on row 9
+       @cash -= amount
+    end
 
     def set_name(name)
         name == nil ? missing_name : @name = name
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -27,14 +27,14 @@ describe Person do
             expect(subject.account).to be_an_instance_of Account
         end
 
-        #it 'with himself as an owner' do
-        #    expect(subject.account.owner).to be subject 
-        #end
+        it 'with himself as an owner' do
+            expect(subject.account.owner).to be subject 
+        end
     end
 
     describe 'can manage funds if an account been created ' do
-        let(:atm) {Atm.new}
-        before {subject.create_account}
+        let(:atm) {Atm.new} # Creating an instance of Atm-class 
+        before {subject.create_account} # Create an account for subject
         
         it 'can deposit funds' do
             expect(subject.deposit(100)).to be_truthy
